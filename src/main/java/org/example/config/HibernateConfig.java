f2cbdd027b61838bcf0fd2d39825d6e09bb1fb12
Shiihon <144337411+Shiihon@users.noreply.github.com>
2024-09-17 11:39:45 +0200

    Entities added to HibernateConfig

--- src/main/java/org/example/config/HibernateConfig.java
+++ src/main/java/org/example/config/HibernateConfig.java
@@ -1,7 +1,10 @@
 package org.example.config;
 
 import jakarta.persistence.EntityManagerFactory;
-import org.example.entities.Person;
+import org.example.entities.CastMember;
+import org.example.entities.Genre;
+import org.example.entities.Movie;
+import org.example.entities.MovieCast;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
@@ -32,7 +35,10 @@ public class HibernateConfig {
     // TODO: IMPORTANT: Add Entity classes here for them to be registered with Hibernate
     private static void getAnnotationConfiguration(Configuration configuration) {
         //configuration.addAnnotatedClass(User.class);
-        configuration.addAnnotatedClass(Person.class);  
+        configuration.addAnnotatedClass(CastMember.class);
+        configuration.addAnnotatedClass(Movie.class);
+        configuration.addAnnotatedClass(Genre.class);
+        configuration.addAnnotatedClass(MovieCast.class);
     }
 
     private static EntityManagerFactory createEMF(boolean forTest, String DBName) {
