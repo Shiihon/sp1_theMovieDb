c98aeaeceb637ecb93c615b141059706fe8bd1fb
Shiihon <144337411+Shiihon@users.noreply.github.com>
2024-09-17 13:58:52 +0200

    added method, MovieById

--- src/main/java/org/example/services/MovieService.java
+++ src/main/java/org/example/services/MovieService.java
@@ -1,6 +1,14 @@
 package org.example.services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.dtos.MovieDTO;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class MovieService {
 
@@ -8,5 +16,35 @@ public class MovieService {
 
     public MovieService(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
+    }
+
+    public MovieDTO getMovieById(int id) {
+        try {
+            HttpClient httpClient = HttpClient.newHttpClient();
+
+            StringBuilder builder = new StringBuilder("https://api.themoviedb.org/3/movie/")
+                    .append(id)
+                    .append("?api_key=")
+                    .append(System.getenv("api_key"));
+
+            HttpRequest request = HttpRequest
+                    .newBuilder()
+                    .header("Accept", "application/json")
+                    .uri(URI.create(builder.toString()))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                MovieDTO movieDTO = objectMapper.readValue(response.body(), MovieDTO.class);
+            } else {
+                System.out.println("GET request failed. Status code: " + response.statusCode());
+            }
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
