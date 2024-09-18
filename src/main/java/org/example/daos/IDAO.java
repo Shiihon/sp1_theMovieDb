495342aa2cbfccae2ad5c043f797ff615bb9695d
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-09-18 15:33:35 +0200

    Format class

--- src/main/java/org/example/daos/IDAO.java
+++ src/main/java/org/example/daos/IDAO.java
@@ -2,10 +2,14 @@ package org.example.daos;
 
 import java.util.Set;
 
-public interface IDAO <T> {
+public interface IDAO<T> {
     T getById(Long id);
+
     Set<T> getAll();
+
     T create(T t);
+
     T update(T t);
+
     void delete(Long id);
 }
