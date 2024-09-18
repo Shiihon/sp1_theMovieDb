10c0f67ba8ba0e80c477a0138bef37808ffcec3d
Liya-ap <144211578+Liya-ap@users.noreply.github.com>
2024-09-18 10:26:45 +0200

    added IDAO interface

---
+++ src/main/java/org/example/daos/IDAO.java
@@ -0,0 +1,11 @@
+package org.example.daos;
+
+import java.util.Set;
+
+public interface IDAO <T> {
+    T getById(Long id);
+    Set<T> getAll();
+    T create(T t);
+    T update(T t);
+    void delete(Long id);
+}
