ea50df8d8f6c6cc2a7ce938bd35b4aaf2c571c4c
Liya-ap <144211578+Liya-ap@users.noreply.github.com>
2024-09-18 13:06:42 +0200

    added constructor for CastMember and CastMemberDTO classes

--- src/main/java/org/example/dtos/CastMemberDTO.java
+++ src/main/java/org/example/dtos/CastMemberDTO.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.entities.CastMember;
+
+import java.util.Objects;
 
 @Data
 @NoArgsConstructor
@@ -16,4 +19,24 @@ public class CastMemberDTO {
     @JsonProperty("known_for_department")
     private String role;
     private String job;
+
+    public CastMemberDTO(CastMember castMember) {
+        this.id = castMember.getId();
+        this.name = castMember.getName();
+        this.role = castMember.getRole();
+        this.job = castMember.getJob();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CastMemberDTO that = (CastMemberDTO) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(role, that.role) && Objects.equals(job, that.job);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, role, job);
+    }
 }
--- src/main/java/org/example/entities/CastMember.java
+++ src/main/java/org/example/entities/CastMember.java
@@ -2,16 +2,22 @@ package org.example.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.dtos.CastMemberDTO;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@NamedQueries({
+        @NamedQuery(name = "CastMember.getAll", query = "SELECT c FROM CastMember c")
+})
 @Entity
 @Table(name = "cast_member")
 public class CastMember {
@@ -20,4 +26,11 @@ public class CastMember {
     private String name;
     private String role;
     private String job;
+
+    public CastMember(CastMemberDTO castMemberDTO) {
+        this.id = castMemberDTO.getId();
+        this.name = castMemberDTO.getName();
+        this.role = castMemberDTO.getRole();
+        this.job = castMemberDTO.getJob();
+    }
 }
