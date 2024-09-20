1a1655df481c9985d36e37f71965498e634f13f5
Liya-ap <cph-op91@cphbusiness.dk>
2024-09-20 17:15:07 +0200

    lombok removed optimized import

--- src/main/java/app/entities/Movie.java
+++ src/main/java/app/entities/Movie.java
@@ -1,10 +1,7 @@
 package app.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.HashSet;
